b66c2235a0a58f8c8f901c32a3c28156ff1bae45
bhavyasimmi <bhavya19agrawal@gmail.com>
2018-05-20 21:19:15 +0530

    option6 and 7updated

--- chose_option.py
+++ chose_option.py
@@ -1,11 +1,21 @@
 #! /usr/bin/python3
 
+import urllib.request
 import time
-from google import search
-
-#from BeautifulSoup import BeautifulSoup
+#from google import search
+#import google
+import search_google
+import os
+import sys
+from sys import platform
+import requests
+import re
+from bs4 import BeautifulSoup
 ## library to query a website ##
-import urllib2
+import webbrowser
+#import urllib2
+import urllib
+
 print ("Press1 : {Enter anything to get related website}")
 print ("Press2 : {Enter anything to get related images}")
 print ("Press3 : {Enter anything to get related URL of webpages}")
@@ -14,41 +24,40 @@ print ("Press5 : {to open default WebBrowser}")
 print ("Press6 : {to check current connected IPs}")
 print ("Press7 : {Enter domain and get owner's name and contact}")
 
-
-option = int(raw_input())
+## using python3 so instead of raw_input() use input() as raw_input() not works here ##
+option = int(input())
 
 if option == 1:
 	print ("option 1 choosen,corresponding output is:")
 	print ("Enter the string")
-	str = raw_input()
+	str = input()
 	list = str.split()
-
-
+	## search for every keyword that is obtained in the splitted list ##
+	for i in list:
+	## function to open new tab for each searched keyword ##
+		webbrowser.open_new_tab('https://www.google.com/search?q='+i)
 
 elif option == 2:
 	print ("option 2 choosen,corresponding output is:")
 	print ("Enter the string")
-	str = raw_input()
+	str = input()
 	list = str.split()
+
+	for i in list:
+		webbrowser.open_new_tab("https://www.google.com/search?biw=1301&bih=670&tbm=isch&sa=1&ei=Fjn9WtLjLpD6rQGigqH4Ag&q="+i)
 
 elif option == 3:
 	print ("option 3 choosen,corresponding output is:")
 	print ("Enter the string")
-	str = raw_input()
-	list = str.split()
-
-	for i in range(0,len(list)):
-		for url in search(list[i], stop=15):
-			## stop = is used for controlling no of output URLs ##
-    			print(url)
-
-		## for getting titles and links of webpages using beautifulsoup and urllib lib ##
-		'''	findPatTitle = re.findall(patFinderTitle,webpage)
-
-			findPatLink = re.findall(patFinderLink,webpage)	
-			print findPatLink	
-		'''
-
+	str = input()
+	list = str.strip().split()
+	for i in list:
+		url_from_web = webbrowser.open_new_tab("https://api.hackertarget.com/pagelinks/?q=https://www.google.com/search?q="+i)		
+		print(url_from_web)
+		#page = requests.get(url_from_web)
+		#soup = Beautifulsoup(page.txt,'html.parser')		
+	
+		
 elif option == 4:
 	print ("option 4 choosen,corresponding output is:")
 	
@@ -57,16 +66,27 @@ elif option == 4:
 
 elif option == 5:
 	print ("option 5 choosen,corresponding output is:")
+	if platform == "linux" or platform == "linux2":
+		print("current operating system is :linux")
+		url = "https://www.mozilla.org/en-US/firefox/central/"
+		webbrowser.open(url)
+	elif platform == "darwin":
+		print("current operating system is: darwin")
+		print("the default browser is:safari")
+	elif platform == "win32":
+		print("current operating system is : windows")
+		print("default browser is Internet Explorer")
 
 elif option == 6:
 	print ("option 6 choosen,corresponding output is:")
 
 
 elif option == 7:
-	print ("option 7 choosen,corresponding output is:")
-	print ("Enter the string")
-	str = raw_input()
-	list = str.split()
+	print ("option 7 choosen,corresponding output is:\n")
+	str = input("Enter the domain_name for which you want to know the owners details     ")
+	list_str = str.split()
+	url = "https://en.wikipedia.org/wiki/"+str
+	webbrowser.open(url)
 
 else:
 	print ("Choose a valid option")
